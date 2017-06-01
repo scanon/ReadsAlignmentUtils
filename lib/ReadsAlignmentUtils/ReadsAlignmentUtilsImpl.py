74c9e9ed21cb5fe49c5c0126095e7bf1d024b7db
U Ganapathy <umaganapathyswork@gmail.com>
2017-06-01 14:47:44 -0400

    Upload/download alignment and tests

--- lib/ReadsAlignmentUtils/ReadsAlignmentUtilsImpl.py
+++ lib/ReadsAlignmentUtils/ReadsAlignmentUtilsImpl.py
@@ -1,5 +1,15 @@
 # -*- coding: utf-8 -*-
 #BEGIN_HEADER
+import os
+import re
+import time
+from pprint import pprint
+from pprint import pformat
+
+from DataFileUtil.DataFileUtilClient import DataFileUtil
+from DataFileUtil.baseclient import ServerError as DFUError
+from ReadsAlignmentUtils.core.sam_tools import SamTools
+
 #END_HEADER
 
 
@@ -24,19 +34,93 @@ the stored alignment.
     # the latter method is running.
     ######################################### noqa
     VERSION = "0.0.1"
-    GIT_URL = "git@github.com:arfathpasha/ReadsAlignmentUtils.git"
-    GIT_COMMIT_HASH = "7daeb26ba8f92cb0a79d2e7e7c6d8240a43a60d2"
+    GIT_URL = "https://github.com/ugswork/ReadsAlignmentUtils.git"
+    GIT_COMMIT_HASH = "bef0d1fb61a7eca8167adcb8ab0c5374c300579d"
 
     #BEGIN_CLASS_HEADER
+
+    PARAM_IN_WS = 'ws_id_or_name'
+    PARAM_IN_OBJ = 'obj_id_or_name'
+    PARAM_IN_FILE = 'file_path'
+
+    INVALID_WS_OBJ_NAME_RE = re.compile('[^\\w\\|._-]')
+    INVALID_WS_NAME_RE = re.compile('[^\\w:._-]')
+
+    def log(self, message, prefix_newline=False):
+        print(('\n' if prefix_newline else '') +
+              str(time.time()) + ': ' + message)
+
+
+    def _check_required_param(self, in_params, param):
+        if (param not in in_params or not in_params[param]):
+            raise ValueError(param + ' parameter is required')
+
+
+    def _proc_ws_obj_params(self, ctx, params):
+
+        dfu = DataFileUtil(self.callback_url, token=ctx['token'])
+
+        if isinstance(params[self.PARAM_IN_WS], int):
+            ws_name_id = params[self.PARAM_IN_WS]
+            # check if ws id is a valid one
+        else:
+            try:
+                ws_name_id = dfu.ws_name_to_id(params[self.PARAM_IN_WS])
+            except DFUError as se:
+                prefix = se.message.split('.')[0]
+                raise ValueError(prefix)
+
+        self.log('Obtained workspace name/id ' + str(ws_name_id))
+
+        # if already created
+        # presence of this object id or name in the workspace is checked later
+
+        obj_name_id = params[self.PARAM_IN_OBJ]
+
+        return ws_name_id, obj_name_id
+
+
+    def _proc_upload_alignment_params(self, ctx, params):
+
+        # TO DO:  add all required params
+
+        self._check_required_param(params, self.PARAM_IN_WS)
+        self._check_required_param(params, self.PARAM_IN_OBJ)
+        self._check_required_param(params, self.PARAM_IN_FILE)
+
+        ws_name_id, obj_name_id = self._proc_ws_obj_params(ctx, params)
+
+        file_path = params.get(self.PARAM_IN_FILE)
+
+        if not (os.path.isfile(file_path)):
+            raise ValueError('File does not exist: ' + file_path)
+
+        return ws_name_id, obj_name_id, file_path
+
+
+    def _proc_download_alignment_params(self, ctx, params):
+
+        self._check_required_param(params, self.PARAM_IN_WS)
+        self._check_required_param(params, self.PARAM_IN_OBJ)
+
+        return self._proc_ws_obj_params(ctx, params)
+
+
+    def _get_aligner_stats(self, file):
+        return self.samtools.get_stats(file)
+
+
     #END_CLASS_HEADER
 
     # config contains contents of config file in a hash or None if it couldn't
     # be found
     def __init__(self, config):
         #BEGIN_CONSTRUCTOR
+        self.scratch = config['scratch']
+        self.callback_url = os.environ['SDK_CALLBACK_URL']
+        self.samtools = SamTools(config)
         #END_CONSTRUCTOR
         pass
-
 
     def validate_alignment(self, ctx, params):
         """
@@ -59,6 +143,7 @@ the stored alignment.
         # return the results
         return [returnVal]
 
+
     def upload_alignment(self, ctx, params):
         """
         Validates and uploads the reads alignment  *
@@ -77,6 +162,67 @@ the stored alignment.
         # ctx is the context object
         # return variables are: returnVal
         #BEGIN upload_alignment
+
+        self.log('Starting upload Reads Alignment, parsing parameters')
+        ws_name_id, obj_name_id, file_path = self._proc_upload_alignment_params(ctx, params)
+
+        # validate input sam/bam file
+
+        dfu = DataFileUtil(self.callback_url, token=ctx['token'])
+
+        uploaded_file = dfu.file_to_shock({'file_path': file_path,
+                                           'make_handle': 1
+                                           })
+
+        file_handle = uploaded_file['handle']
+        file_size = uploaded_file['size']
+
+        print("\nUploaded file ===================")
+        pprint(uploaded_file)
+        print("====================\n")
+
+        aligner_stats = self._get_aligner_stats(file_path)
+
+        # the following parameters are required to be provided for
+        # the workspace object validation
+        #  TO DO:  need to get these from input parameters
+        #  TO_DO:  how to push these into provenance
+
+        aligner_data = {'file': file_handle,
+                       'size': file_size,
+                       #'aligned_using': 'update this',
+                       #'aligner_version': 'update this',
+                       'library_type': 'update this',
+                       'condition': 'update this',
+                       'read_sample_id': '2300',
+                       'genome_id': '3400',
+                       'alignment_stats': aligner_stats
+                      }
+        try:
+
+            res = dfu.save_objects(
+                {"id": ws_name_id,
+                 "objects": [{
+                     "type": "KBaseRNASeq.RNASeqAlignment",
+                     "data": aligner_data,
+                     "name": obj_name_id}
+                 ]})[0]
+
+        except Exception, e:
+            self.log("Failed to save alignment to workspace")
+            raise Exception(e)
+
+        self.log('save complete')
+
+        print("\nSAVE OBJECTS OUTPUT ===================")
+        pprint(res)
+        print("====================\n")
+
+        returnVal = {'obj_ref': str(res[6]) + '/' + str(res[0]) + '/' + str(res[4])}
+
+        print("uploaded object:")
+        print(returnVal)
+
         #END upload_alignment
 
         # At some point might do deeper type checking...
@@ -85,6 +231,7 @@ the stored alignment.
                              'returnVal is not type dict as required.')
         # return the results
         return [returnVal]
+
 
     def export_alignment(self, ctx, params):
         """
@@ -97,6 +244,9 @@ the stored alignment.
         # ctx is the context object
         # return variables are: output
         #BEGIN export_alignment
+
+        #  TO DO
+
         #END export_alignment
 
         # At some point might do deeper type checking...
@@ -105,6 +255,7 @@ the stored alignment.
                              'output is not type dict as required.')
         # return the results
         return [output]
+
 
     def download_alignment(self, ctx, params):
         """
@@ -131,6 +282,55 @@ the stored alignment.
         # ctx is the context object
         # return variables are: returnVal
         #BEGIN download_alignment
+
+        self.log('Running download_alignment with params:\n' +
+                 pformat(params))
+
+        ws_name_id, obj_name_id = self._proc_download_alignment_params(ctx, params)
+
+        dfu = DataFileUtil(self.callback_url, token=ctx['token'])
+
+        obj_ref = obj_name_id if '/' in obj_name_id else \
+                    (str(ws_name_id) + '/' + str(obj_name_id))
+
+        # call get_object_info_new ??
+
+        try:
+            alignment = dfu.get_objects({'object_refs': [obj_ref]})['data']
+        except DFUError as e:
+            self.log('Logging stacktrace from workspace exception:\n' + e.data)
+            raise
+
+        print("Alignment ===============")
+        pprint(alignment)
+        print("=================")
+
+        downloaded_file = alignment[0]['data']['file']['file_name']
+        print(downloaded_file)
+
+        file_name, file_ext = os.path.splitext(downloaded_file)
+        bam_file = ''
+
+        if file_ext.lower() == '.sam':
+            sam_file = downloaded_file
+            bam_file = file_name + '.bam'
+            self.samtools.convert_sam_to_sorted_bam(sam_file, self.scratch,
+                                                        bam_file)
+
+        elif file_ext.lower() == '.bam':
+            bam_file = downloaded_file
+
+        else:
+            raise ValueError("File not of type .sam or .bam")
+
+        bai_file = file_name + '.bai'
+        self.samtools.create_bai_from_bam(bam_file, self.scratch, bai_file)
+
+        returnVal = {'ws_id':ws_name_id,
+                     'bam_file': bam_file,
+                     'bai_file': bai_file
+                     }
+
         #END download_alignment
 
         # At some point might do deeper type checking...
@@ -139,6 +339,7 @@ the stored alignment.
                              'returnVal is not type dict as required.')
         # return the results
         return [returnVal]
+
     def status(self, ctx):
         #BEGIN_STATUS
         returnVal = {'state': "OK",
--- test/ReadsAlignmentUtils_server_test.py
+++ test/ReadsAlignmentUtils_server_test.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 import unittest
 import os  # noqa: F401
-import json  # noqa: F401
 import time
-import requests
+import shutil
+import hashlib
 
 from os import environ
 try:
@@ -47,9 +47,18 @@ class ReadsAlignmentUtilsTest(unittest.TestCase):
                         'authenticated': 1})
         cls.wsURL = cls.cfg['workspace-url']
         cls.wsClient = workspaceService(cls.wsURL)
+        # create workspace
+        wssuffix = int(time.time() * 1000)
+        wsName = "test_alignment_" + str(wssuffix)
+        cls.wsinfo = cls.wsClient.create_workspace({'workspace': wsName})
+        print('created workspace ' + cls.getWsName())
+
         cls.serviceImpl = ReadsAlignmentUtils(cls.cfg)
         cls.scratch = cls.cfg['scratch']
         cls.callback_url = os.environ['SDK_CALLBACK_URL']
+
+        cls.setupTestData()
+
 
     @classmethod
     def tearDownClass(cls):
@@ -57,17 +66,16 @@ class ReadsAlignmentUtilsTest(unittest.TestCase):
             cls.wsClient.delete_workspace({'workspace': cls.wsName})
             print('Test workspace was deleted')
 
+    @classmethod
+    def setupTestData(cls):
+        pass
+
     def getWsClient(self):
         return self.__class__.wsClient
 
-    def getWsName(self):
-        if hasattr(self.__class__, 'wsName'):
-            return self.__class__.wsName
-        suffix = int(time.time() * 1000)
-        wsName = "test_ReadsAlignmentUtils_" + str(suffix)
-        ret = self.getWsClient().create_workspace({'workspace': wsName})  # noqa
-        self.__class__.wsName = wsName
-        return wsName
+    @classmethod
+    def getWsName(cls):
+        return cls.wsinfo[1]
 
     def getImpl(self):
         return self.__class__.serviceImpl
@@ -75,15 +83,132 @@ class ReadsAlignmentUtilsTest(unittest.TestCase):
     def getContext(self):
         return self.__class__.ctx
 
+    @classmethod
+    def md5(self, filename):
+        with open(filename, 'rb') as file_:
+            hash_md5 = hashlib.md5()
+            buf = file_.read(65536)
+            while len(buf) > 0:
+                hash_md5.update(buf)
+                buf = file_.read(65536)
+            return hash_md5.hexdigest()
+
+
     # NOTE: According to Python unittest naming rules test method names should start from 'test'. # noqa
-    def test_your_method(self):
-        # Prepare test objects in workspace if needed using
-        # self.getWsClient().save_objects({'workspace': self.getWsName(),
-        #                                  'objects': []})
-        #
-        # Run your method by
-        # ret = self.getImpl().your_method(self.getContext(), parameters...)
-        #
-        # Check returned data with
-        # self.assertEqual(ret[...], ...) or other unittest methods
-        pass
+
+    # unit test named so that it is called before test_alignment_download()
+
+    def test_alignment_a_upload(self):
+
+        upload_file = 'accepted_hits.sam'
+        upload_path = os.path.join('/kb/module/test/data/samtools', upload_file)
+
+        expected_md5 = self.md5(upload_path)
+
+        shutil.copy2(upload_path, self.scratch)
+        upload_file_path = os.path.join(self.scratch, upload_file)
+
+        params = {'ws_id_or_name': self.getWsName(),
+                  'obj_id_or_name': 'test_alignment',
+                  'file_path': upload_file_path
+                 }
+
+        ret = self.getImpl().upload_alignment(self.ctx, params)[0]
+
+        print("Returned value from upload +++++++++++ ")
+        pprint(ret)
+
+        # TO DO, SOON:  get the file info from the alignment object
+        # Compare file name, size, checksum etc with the given values
+        # they have been manually verified
+
+
+    def test_alignment_download(self):
+
+        params = {'ws_id_or_name': self.getWsName(),
+                  'obj_id_or_name': 'test_alignment'
+                 }
+
+        ret = self.getImpl().download_alignment(self.ctx, params)[0]
+
+        print("Returned value from download +++++++++++ ")
+        pprint(ret)
+
+        # No errors, manually verified
+        # TO DO, SOON: compare downloaded file attributes with that of the uploaded one
+        # another test combining both upload and download
+
+
+    def fail_upload_alignment(self, params, error, exception=ValueError, do_startswith=False):
+        with self.assertRaises(exception) as context:
+            self.getImpl().upload_alignment(self.ctx, params)
+        if do_startswith:
+            self.assertTrue(str(context.exception.message).startswith(error),
+                            "Error message {} does not start with {}".format(
+                                str(context.exception.message),
+                                error))
+        else:
+            self.assertEqual(error, str(context.exception.message))
+
+
+    def test_upload_fail_no_ws(self):
+        self.fail_upload_alignment(
+            {
+             'condition': 'bar',
+             'obj_id_or_name': 'foo',
+             'file_path': 'test'
+             },
+            'ws_id_or_name parameter is required')
+
+
+    def test_upload_fail_no_obj_id(self):
+        self.fail_upload_alignment(
+            {
+             'condition': 'bar',
+             'ws_id_or_name': self.getWsName(),
+             'file_path': 'test'
+             },
+            'obj_id_or_name parameter is required')
+
+
+    def test_upload_fail_no_file(self):
+        self.fail_upload_alignment(
+            {
+             'ws_id_or_name': self.getWsName(),
+             'obj_id_or_name': 'foo'
+             },
+            'file_path parameter is required')
+
+
+    def test_upload_fail_bad_wsname(self):
+        self.fail_upload_alignment(
+            {
+                'ws_id_or_name': '&bad',
+                'obj_id_or_name': 'bar',
+                'file_path': 'foo'
+            },
+            'Illegal character in workspace name &bad: &')
+
+
+    def test_upload_fail_non_existant_wsname(self):
+        self.fail_upload_alignment(
+            {
+             'ws_id_or_name': '1s',
+             'obj_id_or_name': 'foo',
+             'file_path': 'bar'
+             },
+            'No workspace with name 1s exists')
+
+
+    def test_upload_fail_non_existant_file(self):
+        self.fail_upload_alignment(
+            {
+                'ws_id_or_name': self.getWsName(),
+                'obj_id_or_name': 'foo',
+                'file_path': 'foo'
+            },
+            'File does not exist: foo')
+
+
+    # TO DO:  add more tests
+
